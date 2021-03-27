b14afbeab1a45ae7a9aa7f5f39257dcaf7ace12a
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2021-03-27 19:26:44 +0600

    Fix wrong placeholder

--- stubs/App/Http/Controllers/Auth/NewPasswordController.php
+++ stubs/App/Http/Controllers/Auth/NewPasswordController.php
@@ -56,7 +56,7 @@ class NewPasswordController extends Controller
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-                    ? redirect()->route('{{admin}}.login')->with('status', __($status))
+                    ? redirect()->route('{{name}}.login')->with('status', __($status))
                     : back()->withInput($request->only('email'))
                             ->withErrors(['email' => __($status)]);
     }
